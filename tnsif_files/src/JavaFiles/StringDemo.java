d8d8b4f04a9943a3fd44b655650ac4f30e5aa1b1
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:30:11 +0530

    21st

---
+++ tnsif_files/src/JavaFiles/StringDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.capgemini.c2tc.basicsofJava;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String s1="Java string Datatype";
+		System.out.println(s1);
+
+	}
+
+}
