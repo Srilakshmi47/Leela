2bdda4f974280739b45a93ba53e9231679a5102c
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 17:07:14 +0530

    52 commit

---
+++ tnsif_files/lambdaexpressiondemo/WithOutLambdaExp.java
@@ -0,0 +1,24 @@
+package com.tnsif.capgemini.c2tc.lambdaexpressiondemo;
+
+
+	//demo on withoutlambdaexp
+	interface Drawable //functional interface
+	{
+		public void draw(); 
+	}
+	class Test implements Drawable
+	{
+		int width=10;
+		@Override
+		public void draw() {
+			System.out.println("Drawing "+width);	
+		}
+	}
+	public class WithOutLambdaExp {
+
+		public static void main(String[] args) {
+			Drawable d=new Test();
+			d.draw();
+		}
+
+}
