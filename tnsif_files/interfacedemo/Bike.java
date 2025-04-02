68d50c3b46282d13ea0ed8c1ba3a631a90c137f0
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:45:22 +0530

    32nd commit

---
+++ tnsif_files/interfacedemo/Bike.java
@@ -0,0 +1,27 @@
+package com.tnsif.capgemini.c2tc.interfacedemo;
+
+
+public class Bike implements Vehicle
+{
+     private String type;
+     
+     public Bike(String type)
+     {
+    	 this.type=type;
+     }
+     
+	public void start() {
+		
+		System.out.println("Bike " + type + " is starting");
+	}
+
+	public void stop() {
+		
+		System.out.println("Bike " + type+ " is stopping");
+	}
+
+}
+
+	
+
+
