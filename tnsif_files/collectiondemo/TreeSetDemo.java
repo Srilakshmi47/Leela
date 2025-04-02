186195c97ab3a2a2c3c33599ec6b6f6bff3e9e67
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 18:38:51 +0530

    100th commit

---
+++ tnsif_files/collectiondemo/TreeSetDemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.capgemini.collectionsdemo;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		Set<String> set=new TreeSet<>();
+		set.add("cherry");
+		set.add("banana");
+		set.add("apple");
+		
+		set.add("cherry");
+		
+		System.out.println("Treeset " +set);
+	}
+
+
+
+}
