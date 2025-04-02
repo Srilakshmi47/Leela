006a216db8a08bf0c7a83c03b5b3b5e9bbd2d1e3
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:47:29 +0530

    35th

---
+++ tnsif_files/interfacedemo/FunctionalInterface1.java
@@ -0,0 +1,10 @@
+package com.tnsif.capgemini.c2tc.interfacedemo;
+
+
+	import java.lang.FunctionalInterface;
+
+	@FunctionalInterface
+	public interface FunctionalInterface1 {
+	     void sayHello(String name);
+
+}
