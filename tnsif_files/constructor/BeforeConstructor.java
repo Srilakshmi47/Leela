aa0f4dc62412920fe00e05432eb32622c443891e
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:36:18 +0530

    26th commit

---
+++ tnsif_files/constructor/BeforeConstructor.java
@@ -0,0 +1,21 @@
+package com.tnsif.capgemini.c2tc.constructor;
+
+class Myclass
+{
+	String color;
+	int cost;
+	String Brand;
+}
+
+
+public class BeforeConstructor{
+
+	public static void main(String[] args) {
+		Myclass obj=new Myclass();
+		System.out.println(obj.color + " " +obj.cost + " "+obj.Brand);
+
+	}
+
+ 
+
+}
