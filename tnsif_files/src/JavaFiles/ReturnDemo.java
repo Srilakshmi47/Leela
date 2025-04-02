d5bf33e84ea0f59015084bb76239497c20903384
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:26:46 +0530

    18th commit

---
+++ tnsif_files/src/JavaFiles/ReturnDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.capgemini.c2tc.basicsofJava;
+
+public class ReturnDemo {
+	
+	public static void main(String[] args) {
+		System.out.println(sum(5,10));
+		}
+	static int sum(int a,int b)
+	{
+		return a+b;
+	}
+
+
+}
