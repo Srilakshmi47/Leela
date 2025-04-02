c0d6f8c4e6531bcb8bb9f88a529a3d4a62be2292
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:41:45 +0530

    31st commit

---
+++ tnsif_files/FinalDemo/FinalVariable.java
@@ -0,0 +1,13 @@
+package com.tnsif.capgemini.c2tc.finaldemo;
+
+public class FinalVariable {
+	
+	public static void main(String[] args) {
+		int  mark_scored_leela=50;
+		mark_scored_leela=80; //final variable can't be changed
+		System.out.println(mark_scored_leela);
+
+	}
+
+
+}
