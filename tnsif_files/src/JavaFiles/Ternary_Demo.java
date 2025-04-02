5dd78d387f665bbae9f25addad23cf38d87c0799
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:31:21 +0530

    23rd

---
+++ tnsif_files/src/JavaFiles/Ternary_Demo.java
@@ -0,0 +1,13 @@
+package com.tnsif.capgemini.c2tc.basicsofJava;
+
+public class Ternary_Demo {
+	
+	public static void main(String[] args) {
+		int a=10,b=5;
+		int max=(a>b)?a:b;
+		System.out.println("max value is : " + max);
+
+	}
+
+
+}
