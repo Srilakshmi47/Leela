f7b512d676a149cd0ac60722016a6488c331e8a9
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:40:07 +0530

    29th commit

---
+++ tnsif_files/FinalDemo/Finalclass.java
@@ -0,0 +1,33 @@
+package com.tnsif.capgemini.c2tc.finaldemo;
+
+
+	class calc
+	{
+		public void show()
+		{
+			System.out.println("AI integration done by hema");
+		}
+	}
+
+	class adav extends  calc 
+	{
+		public void show()
+		{
+			System.out.println("AI integration done by yasar");
+		}
+	}
+
+
+
+	public class Finalclass {
+
+		public static void main(String[] args) {
+			
+
+		}
+
+}
+
+
+
+
