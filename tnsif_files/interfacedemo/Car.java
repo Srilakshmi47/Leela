0c23eb34e98537fefb622ac6c8b32fdc054ecd9c
Srilakshmi47 <sreelakshmikarri28@gmail.com>
2025-04-02 15:46:25 +0530

    33rd

---
+++ tnsif_files/interfacedemo/Car.java
@@ -0,0 +1,28 @@
+package com.tnsif.capgemini.c2tc.interfacedemo;
+
+	public class Car implements Vehicle
+	{
+	    private String model;
+	    
+	    
+	    //constructor
+	          
+	   public Car(String model)
+	   {
+		   this.model=model;
+	   }
+	     
+		@Override
+		public  void start() {
+			
+			System.out.println("Car " + model + " is starting");
+		}
+
+		@Override
+		public void stop() {
+			System.out.println("Car " +model +" is stopped");
+			
+		}
+
+
+}
